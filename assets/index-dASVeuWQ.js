a0e95fab39a0c34f78a882607ffca804f1fc6dc6
Dfdx Alex <alexmway78@gmail.com>
2026-09-24 15:51:15 +0200

    Updates

--- assets/index-dASVeuWQ.js
+++ assets/index-dASVeuWQ.js
@@ -80,7 +80,7 @@ var e=(e,t,n)=>()=>{if(n)throw n[0];try{return e&&(t=e(e=0)),t}catch(e){throw n=
                     `,``)}
             </div>
         </section>
-    `}render(e){document.querySelector(`#quick-add`).innerHTML=this.#e(e)}bindEvents(){let e=document.querySelectorAll(`[data-category-btn]`);console.log(e[0].innerText),console.log(e[1].innerText),e[0].addEventListener(`click`,()=>{document.body.innerHTML=this.quickMenuItemsWievCategor}),document.addEventListener(`click`,e=>{})}injectQuickMenuItemsWievCategor(e){this.quickMenuItemsWievCategor=e}}})),h,g=e((()=>{h=class{startProducts(){return[{icon:`🥚`,name:`Яйцо`,protein:12.5,fat:10.5,carbs:1,fiber:0,calories:155},{icon:`🍗`,name:`Курица`,protein:23,fat:2,carbs:0,fiber:0,calories:110},{icon:`🥛`,name:`Творог`,protein:18,fat:5,carbs:3,fiber:0,calories:145},{icon:`➕`,name:`Добавить`,protein:``,fat:0,carbs:0,fiber:0,calories:0},{icon:`➖`,name:`Убрать`,protein:``,fat:0,carbs:0,fiber:0,calories:0}]}meatProducts(){return[{name:`Куриная грудка`,icon:`🍗`,protein:23,fat:2,carbs:0,fiber:0,calories:110},{name:`Куриное бедро`,icon:`🍖`,protein:19,fat:9,carbs:0,fiber:0,calories:170},{name:`Куриные крылья`,icon:`🪽`,protein:18,fat:14,carbs:0,fiber:0,calories:210},{name:`Индейка филе`,icon:`🦃`,protein:22,fat:4,carbs:0,fiber:0,calories:120},{name:`Говядина постная`,icon:`🥩`,protein:26,fat:8,carbs:0,fiber:0,calories:160},{name:`Говяжий фарш`,icon:`🍖`,protein:17,fat:15,carbs:0,fiber:0,calories:215},{name:`Стейк рибай`,icon:`🦬`,protein:24,fat:20,carbs:0,fiber:0,calories:290},{name:`Свинина постная`,icon:`🐖`,protein:21,fat:6,carbs:0,fiber:0,calories:140},{name:`Свиной стейк`,icon:`🍖`,protein:20,fat:12,carbs:0,fiber:0,calories:200},{name:`Бекон`,icon:`🥓`,protein:14,fat:42,carbs:1,fiber:0,calories:450},{name:`Сосиски`,icon:`🌭`,protein:12,fat:20,carbs:2,fiber:0,calories:250},{name:`Колбаса`,icon:`🥙`,protein:17,fat:25,carbs:2,fiber:0,calories:300},{name:`Ветчина`,icon:`🍖`,protein:18,fat:6,carbs:1,fiber:0,calories:135},{name:`Котлета мясная`,icon:`🍔`,protein:16,fat:14,carbs:5,fiber:0,calories:220},{name:`Фарш смешанный`,icon:`🧆`,protein:17,fat:18,carbs:0,fiber:0,calories:240},{name:`Лосось`,icon:`🐟`,protein:20,fat:13,carbs:0,fiber:0,calories:208},{name:`Тунец`,icon:`🐠`,protein:23,fat:1,carbs:0,fiber:0,calories:110},{name:`Треска`,icon:`🐡`,protein:18,fat:1,carbs:0,fiber:0,calories:82},{name:`Скумбрия`,icon:`🐋`,protein:19,fat:14,carbs:0,fiber:0,calories:205},{name:`Креветки`,icon:`🦐`,protein:24,fat:.5,carbs:0,fiber:0,calories:99},{name:`Мидии`,icon:`🦪`,protein:12,fat:2,carbs:3,fiber:0,calories:86},{name:`Яйца куриные`,icon:`🥚`,protein:12.5,fat:10.5,carbs:1,fiber:0,calories:155},{name:`Яичный белок`,icon:`⚪`,protein:11,fat:0,carbs:.7,fiber:0,calories:48},{name:`Яичный желток`,icon:`🟡`,protein:16,fat:27,carbs:3.5,fiber:0,calories:322},{name:`Печень куриная`,icon:`🫀`,protein:20,fat:6,carbs:1,fiber:0,calories:135},{name:`Печень говяжья`,icon:`🫁`,protein:21,fat:3.5,carbs:4,fiber:0,calories:140},{name:`Сердце куриное`,icon:`❤️`,protein:16,fat:9,carbs:0,fiber:0,calories:160},{name:`Сердце говяжье`,icon:`💗`,protein:17,fat:4,carbs:0,fiber:0,calories:120},{name:`Почки`,icon:`🧫`,protein:17,fat:3,carbs:0,fiber:0,calories:100}]}}})),_,v=e((()=>{_=class{get(e){let t=localStorage.getItem(e);if(t===null)return null;try{return JSON.parse(t)}catch{return null}}set(e,t){localStorage.setItem(e,JSON.stringify(t))}remove(e){localStorage.removeItem(e)}static help(){console.log(`
+    `}render(e){document.querySelector(`#quick-add`).innerHTML=this.#e(e)}bindEvents(){document.querySelectorAll(`[data-category-btn]`).forEach(e=>{e.addEventListener(`click`,()=>{document.body.innerHTML=this.quickMenuItemsWievCategor})})}injectQuickMenuItemsWievCategor(e){this.quickMenuItemsWievCategor=e}}})),h,g=e((()=>{h=class{startProducts(){return[{icon:`🥚`,name:`Яйцо`,protein:12.5,fat:10.5,carbs:1,fiber:0,calories:155},{icon:`🍗`,name:`Курица`,protein:23,fat:2,carbs:0,fiber:0,calories:110},{icon:`🥛`,name:`Творог`,protein:18,fat:5,carbs:3,fiber:0,calories:145},{icon:`➕`,name:`Добавить`,protein:``,fat:0,carbs:0,fiber:0,calories:0},{icon:`➖`,name:`Убрать`,protein:``,fat:0,carbs:0,fiber:0,calories:0}]}meatProducts(){return[{name:`Куриная грудка`,icon:`🍗`,protein:23,fat:2,carbs:0,fiber:0,calories:110},{name:`Куриное бедро`,icon:`🍖`,protein:19,fat:9,carbs:0,fiber:0,calories:170},{name:`Куриные крылья`,icon:`🪽`,protein:18,fat:14,carbs:0,fiber:0,calories:210},{name:`Индейка филе`,icon:`🦃`,protein:22,fat:4,carbs:0,fiber:0,calories:120},{name:`Говядина постная`,icon:`🥩`,protein:26,fat:8,carbs:0,fiber:0,calories:160},{name:`Говяжий фарш`,icon:`🍖`,protein:17,fat:15,carbs:0,fiber:0,calories:215},{name:`Стейк рибай`,icon:`🦬`,protein:24,fat:20,carbs:0,fiber:0,calories:290},{name:`Свинина постная`,icon:`🐖`,protein:21,fat:6,carbs:0,fiber:0,calories:140},{name:`Свиной стейк`,icon:`🍖`,protein:20,fat:12,carbs:0,fiber:0,calories:200},{name:`Бекон`,icon:`🥓`,protein:14,fat:42,carbs:1,fiber:0,calories:450},{name:`Сосиски`,icon:`🌭`,protein:12,fat:20,carbs:2,fiber:0,calories:250},{name:`Колбаса`,icon:`🥙`,protein:17,fat:25,carbs:2,fiber:0,calories:300},{name:`Ветчина`,icon:`🍖`,protein:18,fat:6,carbs:1,fiber:0,calories:135},{name:`Котлета мясная`,icon:`🍔`,protein:16,fat:14,carbs:5,fiber:0,calories:220},{name:`Фарш смешанный`,icon:`🧆`,protein:17,fat:18,carbs:0,fiber:0,calories:240},{name:`Лосось`,icon:`🐟`,protein:20,fat:13,carbs:0,fiber:0,calories:208},{name:`Тунец`,icon:`🐠`,protein:23,fat:1,carbs:0,fiber:0,calories:110},{name:`Треска`,icon:`🐡`,protein:18,fat:1,carbs:0,fiber:0,calories:82},{name:`Скумбрия`,icon:`🐋`,protein:19,fat:14,carbs:0,fiber:0,calories:205},{name:`Креветки`,icon:`🦐`,protein:24,fat:.5,carbs:0,fiber:0,calories:99},{name:`Мидии`,icon:`🦪`,protein:12,fat:2,carbs:3,fiber:0,calories:86},{name:`Яйца куриные`,icon:`🥚`,protein:12.5,fat:10.5,carbs:1,fiber:0,calories:155},{name:`Яичный белок`,icon:`⚪`,protein:11,fat:0,carbs:.7,fiber:0,calories:48},{name:`Яичный желток`,icon:`🟡`,protein:16,fat:27,carbs:3.5,fiber:0,calories:322},{name:`Печень куриная`,icon:`🫀`,protein:20,fat:6,carbs:1,fiber:0,calories:135},{name:`Печень говяжья`,icon:`🫁`,protein:21,fat:3.5,carbs:4,fiber:0,calories:140},{name:`Сердце куриное`,icon:`❤️`,protein:16,fat:9,carbs:0,fiber:0,calories:160},{name:`Сердце говяжье`,icon:`💗`,protein:17,fat:4,carbs:0,fiber:0,calories:120},{name:`Почки`,icon:`🧫`,protein:17,fat:3,carbs:0,fiber:0,calories:100}]}}})),_,v=e((()=>{_=class{get(e){let t=localStorage.getItem(e);if(t===null)return null;try{return JSON.parse(t)}catch{return null}}set(e,t){localStorage.setItem(e,JSON.stringify(t))}remove(e){localStorage.removeItem(e)}static help(){console.log(`
             тупой класс для работы с локальным хранилищем
             никаких мыслей сюда не вкладывать!!!
             get возвращает либо null либо готовый для работы массив
@@ -107,50 +107,50 @@ var e=(e,t,n)=>()=>{if(n)throw n[0];try{return e&&(t=e(e=0)),t}catch(e){throw n=
             
                 <ul class="category-list">
                     <li class="category-item">
-                        <label>
-                            <input type="checkbox" value="meat">
+                        <label class="category-label">
+                            <input class="category-checkbox" type="checkbox" value="meat">
                             <span class="category-name">Мясо</span>
                         </label>
                     </li>
 
                     <li class="category-item">
-                        <label>
-                            <input type="checkbox" value="fish">
+                        <label class="category-label">
+                            <input class="category-checkbox" type="checkbox" value="fish">
                             <span class="category-name">Рыба</span>
                         </label>
                     </li>
 
                     <li class="category-item">
-                        <label>
-                            <input type="checkbox" value="seafood">
+                        <label class="category-label">
+                            <input class="category-checkbox" type="checkbox" value="seafood">
                             <span class="category-name">Морепродукты</span>
                         </label>
                     </li>
 
                     <li class="category-item">
-                        <label>
-                            <input type="checkbox" value="eggs">
+                        <label class="category-label">
+                            <input class="category-checkbox" type="checkbox" value="eggs">
                             <span class="category-name">Яйца</span>
                         </label>
                     </li>
 
                     <li class="category-item">
-                        <label>
-                            <input type="checkbox" value="dairy">
+                        <label class="category-label">
+                            <input class="category-checkbox" type="checkbox" value="dairy">
                             <span class="category-name">Молочка</span>
                         </label>
                     </li>
 
                     <li class="category-item">
-                        <label>
-                            <input type="checkbox" value="vegan">
+                        <label class="category-label">
+                            <input class="category-checkbox" type="checkbox" value="vegan">
                             <span class="category-name">Растительные белки</span>
                         </label>
                     </li>
 
                     <li class="category-item">
-                        <label>
-                            <input type="checkbox" value="vegan">
+                        <label class="category-label">
+                            <input class="category-checkbox" type="checkbox" value="vegan">
                             <span class="category-name">Вредное</span>
                         </label>
                     </li>
@@ -159,8 +159,5 @@ var e=(e,t,n)=>()=>{if(n)throw n[0];try{return e&&(t=e(e=0)),t}catch(e){throw n=
                 <span class="cat-icon">Показать</span>
             </button>
             </div>
-
         </div>
-
-
         `}}})),w,T=e((()=>{m(),b(),C(),w=class{constructor(){this.quickAdd=new p,this.quickMenuModel=new y,this.quickMenuItemsWievCategor=new S}init(){let e=this.quickMenuModel.get(`quickMenu`);this.quickAdd.render(e),this.quickAdd.injectQuickMenuItemsWievCategor(this.quickMenuItemsWievCategor.render()),this.quickAdd.bindEvents()}}})),E,D=e((()=>{g(),v(),E=class{constructor(){this.products=new h,this.storageService=new _}init(){if(this.storageService.get(`quickMenu`)===null){let e=this.products.startProducts();this.storageService.set(`quickMenu`,e)}}}}));t((()=>{o(),d(),T(),D(),new E().init(),new a().init(),new u().init(),new w().init()}))();
